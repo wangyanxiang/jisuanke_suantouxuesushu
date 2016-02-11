4930fa7cf18151100e9b25b15c51e7c8307a9b08
wangyanxiang <2267143778@qq.com>
2016-02-11 12:49:30 +0800

    网上别人的代码，思路大概都差不多，主要还是符号实现上有一点区别，但是结果还是一样的，就是不知道能不能在计蒜客网站上通过

--- cal.cpp
+++ cal.cpp
@@ -1,48 +1,32 @@
-//原始有错版本，一旦得到正确的输出之后，found标志位就无法变回false，导致无法产生正确的结果
-#include <bits/stdc++.h>
-using namespace std;
-int n; // 保存读入的整数N
-bool opr[10]; // 可以用一个bool数组保存所有n-1个符号，我们用true表示加号"+"，用false表示减号"-"。
-bool found = false; // 表示是否找到一组解，如果没找到的话需要在最后输出"None"
-// dfs函数，保存两个状态：deep表示递归深度，就是枚举到第几个数字左边的符号；sum表示之前部分表达式的值。
-// 如果枚举完成后sum的值刚好为0，则输出这组方案。我们需要在搜索的过程中用opr数组保存状态。
-void dfs(int deep, int sum) {
-    // 请在下面的条件表达式中填上对应的值。
-    if (deep == n) {
-        if (sum == 0) {
-            found = true;
-            // 请在下面将最终方案输出，方案格式参照题目描述。
-            // 最后记得换行哦。
-            printf("1");
-            int i = 1;
-            for(i = 1; i < n; i++){
-                if (opr[i] == true)
-                    printf("+");
-                else
-                    printf("-");
-                printf("%d", i + 1);
-            }
-            printf("\n");
-        }
-        return ;
-    }
-    // 请在下面的语句中填出正确的逻辑。
-    opr[deep] = true;
-    dfs( deep + 1  , sum + deep + 1  );
-    opr[deep] = false;
-    dfs( deep + 1  , sum - deep - 1  );
+#include<iostream>  
+#include<algorithm>  
+#include<cmath>  
+#include<cstdio>  
+using namespace std;  
+int n,i,fg;  
+char a[22];  
+void dfs(int idx,int ret)
+{  
+    if(idx==n){  
+            if(ret==0){  
+	              fg=1;  
+			      printf("1");  
+				  for(i=1;i<=n-1;i++)  
+					printf("%c%d",a[i],i+1);  
+				  puts("");  
+				  } 
+			 return;
+			 }  
+			 a[idx]='+';  
+			 dfs(idx+1,ret+idx+1);  										             a[idx]='-';  
+			 dfs(idx+1,ret-(idx+1));  
 }
-
-// main函数请自己完成哦，加油~
-int main() {
-    while(scanf("%d", &n) == 1){
-        if(n == 0){
-            printf("NONE");
-            return 0;
-        }
-        dfs(1, 1);
-        if(found == false)
-            printf("NONE");
-    }
-    return 0;
-}
+int main(){  
+    while(scanf("%d",&n)==1){  
+	        fg=0;  
+			dfs(1,1);  
+			if(!fg)  
+				printf("None\n");  
+	} 
+	return 0;  
+} 
